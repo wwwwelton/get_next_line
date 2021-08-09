9614b1a5bc1654995752c78af4f1c291a455bd7a
wleite <wleite@student.42sp.org.br>
2021-08-09 00:14:44 -0300

    feat: add GNL prototype to header

---
+++ get_next_line.h
@@ -0,0 +1,21 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: wleite <wleite@student.42sp.org.br>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/08/09 00:12:59 by wleite            #+#    #+#             */
+/*   Updated: 2021/08/09 00:14:03 by wleite           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+# include <unistd.h>
+# include <stdlib.h>
+
+char	*get_next_line(int fd);
+
+#endif
