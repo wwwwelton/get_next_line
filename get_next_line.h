73ed03e8bb7d328fdc8177918735e53ab3b09bbd
wleite <wleite@student.42sp.org.br>
2021-08-16 23:53:12 -0300

    feat: add OPEN_MAX def to handle multiple files

--- get_next_line.h
+++ get_next_line.h
@@ -6,7 +6,7 @@
 /*   By: wleite <wleite@student.42sp.org.br>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/08/09 00:12:59 by wleite            #+#    #+#             */
-/*   Updated: 2021/08/13 21:56:55 by wleite           ###   ########.fr       */
+/*   Updated: 2021/08/16 18:33:41 by wleite           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,21 +16,19 @@
 # include <unistd.h>
 # include <stdlib.h>
 
+#ifndef OPEN_MAX
+# define OPEN_MAX 256
+#endif
+
 #ifndef BUFFER_SIZE
 # define BUFFER_SIZE 10
 #endif
 
-char	*get_next_line(int fd);
-char	*ft_substr(char const *s, unsigned int start, size_t len);
-char	*ft_strdup(const char *str);
-char	*ft_strchr(const char *s, int c);
-char	*ft_strjoin(char const *s1, char const *s2);
-size_t	ft_strlcpy(char *dst, const char *src, size_t dstsize);
-char	*get_line(int fd, char **buffer, char **buffer_backup);
-char	*extract_line(char **buffer_backup);
-
-void	*ft_memcpy(void *dst, const void *src, size_t len);
-void	*ft_memmove(void *dst, const void *src, size_t len);
 size_t	ft_strlen(const char *s);
+char	*ft_strchr(const char *s, int c);
+char	*ft_strdup(const char *str);
+char	*ft_strjoin(char const *s1, char const *s2);
+char	*ft_substr(char const *s, unsigned int start, size_t len);
+char	*get_next_line(int fd);
 
 #endif
