9e0dad8e21f044f5796ef5b9795cbe99a3a206a6
wleite <wleite@student.42sp.org.br>
2021-08-16 23:52:24 -0300

    refactor: remove external dependencies

--- get_next_line_utils.c
+++ get_next_line_utils.c
@@ -6,7 +6,7 @@
 /*   By: wleite <wleite@student.42sp.org.br>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/08/09 00:15:00 by wleite            #+#    #+#             */
-/*   Updated: 2021/08/12 22:38:53 by wleite           ###   ########.fr       */
+/*   Updated: 2021/08/16 18:38:05 by wleite           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,46 +22,46 @@ size_t	ft_strlen(const char *s)
 	return (i);
 }
 
-static size_t	strnlen(const char *s, size_t n)
-{
-	size_t	i;
-
-	i = 0;
-	while (s[i] && i < n)
-		i++;
-	return (i);
-}
-
-char	*ft_substr(char const *s, unsigned int start, size_t len)
-{
-	char	*str;
-	size_t	srclen;
-
-	if (!s)
-		return (NULL);
-	srclen = ft_strlen(s);
-	if (srclen > start)
-		len = strnlen(&s[start], len);
-	else
-		return (ft_strdup(""));
-	str = (char *)malloc(sizeof(char) * len + 1);
-	if (!str)
-		return (NULL);
-	ft_strlcpy(str, &s[start], len + 1);
-	return (str);
-}
-
 char	*ft_strdup(const char *str)
 {
-	char	*s;
+	size_t	i;
 	size_t	len;
+	char	*s;
 
 	len = ft_strlen(str) + 1;
 	s = (char *)malloc(sizeof(char) * len);
 	if (s == NULL)
 		return (NULL);
-	ft_strlcpy(s, str, len);
+	i = -1;
+	while (++i < len)
+		s[i] = str[i];
 	return (s);
+}
+
+char	*ft_substr(char const *s, unsigned int start, size_t len)
+{
+	size_t	i;
+	size_t	srclen;
+	char	*str;
+
+	if (!s)
+		return (NULL);
+	srclen = ft_strlen(s);
+	if (start > srclen)
+		return (ft_strdup(""));
+	if (start + len > srclen)
+		len = srclen - start;
+	str = (char *)malloc(sizeof(char) * len + 1);
+	if (!str)
+		return (NULL);
+	i = 0;
+	while (s[start + i] && i < len)
+	{
+		str[i] = s[start + i];
+		i++;
+	}
+	str[i] = '\0';
+	return (str);
 }
 
 char	*ft_strchr(const char *s, int c)
@@ -82,53 +82,11 @@ char	*ft_strchr(const char *s, int c)
 	return (NULL);
 }
 
-void	*ft_memcpy(void *dst, const void *src, size_t len)
-{
-	unsigned char	*ptr_dest;
-	unsigned char	*ptr_src;
-
-	if ((!dst && !src) || !len)
-		return (dst);
-	ptr_dest = (unsigned char *)dst;
-	ptr_src = (unsigned char *)src;
-	while (len--)
-		*ptr_dest++ = *ptr_src++;
-	return (dst);
-}
-
-size_t	ft_strlcpy(char *dst, const char *src, size_t dstsize)
-{
-	size_t	srclen;
-	size_t	len;
-
-	if (!dst || !src)
-		return (0);
-	srclen = ft_strlen(src);
-	if (dstsize)
-	{
-		len = strnlen(src, dstsize - 1);
-		ft_memcpy(dst, src, len);
-		dst[len] = '\0';
-	}
-	return (srclen);
-}
-
-size_t	ft_strlcat(char *dst, const char *src, size_t dstsize)
-{
-	size_t	srclen;
-	size_t	dstlen;
-
-	srclen = ft_strlen(src);
-	dstlen = strnlen(dst, dstsize);
-	if (dstlen == dstsize)
-		return (dstlen + srclen);
-	ft_strlcpy(&dst[dstlen], src, dstsize - dstlen);
-	return (dstlen + srclen);
-}
-
 char	*ft_strjoin(char const *s1, char const *s2)
 {
 	char	*str;
+	size_t	i;
+	size_t	j;
 	size_t	size;
 
 	if (!s1 || !s2)
@@ -137,31 +95,12 @@ char	*ft_strjoin(char const *s1, char const *s2)
 	str = (char *)malloc(sizeof(char) * size);
 	if (!str)
 		return (NULL);
-	ft_strlcpy(str, s1, ft_strlen(s1) + 1);
-	ft_strlcat(str, s2, size);
+	i = -1;
+	while (s1[++i])
+		str[i] = s1[i];
+	j = -1;
+	while (s2[++j])
+		str[i + j] = s2[j];
+	str[i + j] = '\0';
 	return (str);
-}
-
-static void	*memrcpy(void *dst, const void *src, size_t len)
-{
-	unsigned char	*ptr_dest;
-	unsigned char	*ptr_src;
-
-	if ((!dst && !src) || !len)
-		return (dst);
-	ptr_dest = (unsigned char *)dst + len - 1;
-	ptr_src = (unsigned char *)src + len - 1;
-	while (len--)
-		*ptr_dest-- = *ptr_src--;
-	return (dst);
-}
-
-void	*ft_memmove(void *dst, const void *src, size_t len)
-{
-	if ((!dst && !src) || !len)
-		return (dst);
-	if (dst <= src)
-		return (ft_memcpy(dst, src, len));
-	else
-		return (memrcpy(dst, src, len));
 }
